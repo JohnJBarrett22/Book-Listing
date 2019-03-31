5ba9a722fc0f815092b0bf83c80195f7519e78e4
John Barrett <johnjbarrett22@gmail.com>
2019-03-30 17:18:50 -0700

    Removed logo file/reference and added components folder with a data.js file

---
+++ src/components/data.js
@@ -0,0 +1,22 @@
+const books = [
+	{
+		id: 1,
+		img: 'https://m.media-amazon.com/images/I/91q-zsJV6rL._SX140_.jpg',
+		title: 'The Reckoning',
+		author: 'John Grisham'
+	},
+	{
+		id: 2,
+		img: 'https://m.media-amazon.com/images/I/91-oYFNIJ9L._SX140_.jpg',
+		title: 'The Wonky Donky',
+		author: 'Craig Smith and Katz Cowley'
+	},
+	{
+		id: 3,
+		img: 'https://m.media-amazon.com/images/I/91G2aMQuu9L._SX140_.jpg',
+		title: "Harry Potter and the Sorceror's Stone",
+		author: 'J.K. Rowling'
+	}
+];
+
+export default books;
